9fcac85c9afff59a49354b64fa6b9ee46b3c3115
Zhou <zyang62@gmail.com>
2016-02-17 01:12:10 -0500

    need to work

--- bookreposit.py
+++ bookreposit.py
@@ -24,14 +24,12 @@ def reposit_key(genre=DEFAULT_GENRE):
     return ndb.Key('genre', genre)
 
 
-class Author(ndb.Model):
-    """Sub model for representing an author."""
-    identity = ndb.StringProperty(indexed=False)
 
 
 class Greeting(ndb.Model):
     """A main model for representing an individual Guestbook entry."""
-    author = ndb.StructuredProperty(Author)
+    
+    author = ndb.StringProperty(indexed=False)
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
@@ -66,7 +64,7 @@ class Reposit(webapp2.RequestHandler):
         greeting = Greeting(parent=reposit_key(genre))
 
 
-        greeting.identity = self.request.get('author')
+        greeting.author = self.request.get('author')
         greeting.content = self.request.get('content')
         greeting.put()
 
@@ -136,15 +134,60 @@ class Enter(webapp2.RequestHandler):
         greeting = Greeting(parent=reposit_key(genre))
 
 
-        greeting.identity = self.request.get('author')
+        greeting.author = self.request.get('author')
         greeting.content = self.request.get('content')
         greeting.put()
 
         query_params = {'genre': genre}
         self.redirect('/?' + urllib.urlencode(query_params))
 
+class Search(webapp2.RequestHandler):
+
+    def post(self):
+        # We set the same parent key on the 'Greeting' to ensure each
+        # Greeting is in the same entity group. Queries across the
+        # single entity group will be consistent. However, the write
+        # rate to a single entity group should be limited to
+        # ~1/second.
+        genre = self.request.get('genre',
+                                          DEFAULT_GENRE)
+        greeting = Greeting(parent=reposit_key(genre))
+
+
+        greeting.author = self.request.get('author')
+        greeting.content = self.request.get('content')
+        greeting.put()
+
+        query_params = {'genre': genre}
+        self.redirect('/?' + urllib.urlencode(query_params))
+
+    def get(self):
+        genre = self.request.get('genre',DEFAULT_GENRE)
+        greetings_query = Greeting.query(
+            ancestor=reposit_key(genre)).order(-Greeting.date)
+        greetings = greetings_query.fetch(10)
+
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'user': user,
+            'greetings': greetings,
+            'genre': urllib.quote_plus(genre),
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('search.html')
+        self.response.write(template.render(template_values))
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/enter', Enter),
     ('/display',Reposit),
+    ('/search',Search),
 ], debug=True)
